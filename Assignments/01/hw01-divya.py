dc50b4b838bb7fae4ca96558011f7d2efcd0a783
Div <rasaniadivya1@gmail.com>
2025-02-03 15:58:58 -0500

    Assignment 01

---
+++ Assignments/01/hw01-divya.py
@@ -0,0 +1,10 @@
+# C200 / Spring 2025
+# Homework Assignment 01
+# Divya Rasania
+# drasania
+
+for i in range(0,3):
+    print('First Name: Divya')
+    print("Last Name: Rasania")
+
+print("""Nickname from a videogame: notsodivine""")
---
+++ Assignments/01/hw01-title.py
@@ -0,0 +1,7 @@
+# C200 / Spring 2025
+# Homework Assignment 01
+# Divya Rasania
+# drasania
+
+print('A book: ' + '"Ender\'s Game"')
+print("""A credited person: """ + "Orson Scott Card")
